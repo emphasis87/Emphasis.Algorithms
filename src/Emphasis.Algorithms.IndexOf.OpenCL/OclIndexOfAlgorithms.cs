dd4080449df576cb47231ff809b14da4a43e8ec6
lubos_hemala <lubos.hemala@gmail.com>
2021-05-30 22:02:17 +0200

    ocl index of

--- src/Emphasis.Algorithms.IndexOf.OpenCL/OclIndexOfAlgorithms.cs
+++ src/Emphasis.Algorithms.IndexOf.OpenCL/OclIndexOfAlgorithms.cs
@@ -1,9 +1,21 @@
 ﻿using System;
+using Emphasis.OpenCL;
 
 namespace Emphasis.Algorithms.IndexOf.OpenCL
 {
 	public class OclIndexOfAlgorithms
 	{
+		public bool IsSupported(nint deviceId)
+		{
+			var extensions = OclHelper.GetDeviceExtensions(deviceId);
+			return 
+				extensions.Contains("cl_khr_int64_base_atomics") ||
+				extensions.Contains("cl_khr_global_int32_base_atomics") && extensions.Contains("cl_khr_local_int32_base_atomics ");
+		}
 
+		public int IndexOfGreaterThan(int width, int height, nint sourceBufferId, nint indexesBufferId, int comparand)
+		{
+
+		}
 	}
 }
