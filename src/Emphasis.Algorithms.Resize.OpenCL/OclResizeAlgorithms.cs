39934c8f984a4f3b78d7f8301fc0e8479458daa6
lubos_hemala <lubos.hemala@gmail.com>
2021-07-18 00:42:06 +0200

    resize

--- src/Emphasis.Algorithms.Resize.OpenCL/OclResizeAlgorithms.cs
+++ src/Emphasis.Algorithms.Resize.OpenCL/OclResizeAlgorithms.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Threading.Tasks;
+using Emphasis.OpenCL;
 
 namespace Emphasis.Algorithms.Resize.OpenCL
 {
@@ -10,6 +12,20 @@ namespace Emphasis.Algorithms.Resize.OpenCL
 
 	public class OclResizeAlgorithms : IOclResizeAlgorithms
 	{
-		
+		private readonly ConcurrentDictionary<(nint queueId, string args), (nint contextId, nint deviceId, nint programId)> _programs = new();
+
+		public async Task<nint> BiLinear(
+			nint queueId,
+			int width0,
+			int height0,
+			int width1,
+			int height1,
+			OclTypedBuffer sourceBuffer,
+			OclTypedBuffer resultBuffer)
+		{
+			var x_ratio = (float) (width0 - 1) / width1;
+			var y_ratio = (float) (height0 - 1) / height1;
+			
+		}
 	}
 }
